523ad94bd42705625a6639891d12ca7efe3a2b97
alexandre katamadze <163563288+alexandre-katamadze-Davit-Janashias@users.noreply.github.com>
2024-03-17 00:26:16 +0400

    Add files via upload

---
+++ Goa/day6/day6.py
@@ -0,0 +1,7 @@
+name = input('Enter your name: ')
+last_name = input('Enter your last name: ')
+age = input('Enter your age: ')
+
+
+
+print(age,last_name,name)
---
+++ Goa/day6/lesson6.py
@@ -0,0 +1,4 @@
+age = input('Enter your age: ')
+future_age = input(25)
+
+print(age + future_age)
