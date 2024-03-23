17ab33a0e8553dabc06b85393ffa893cf8748df9
alexandre katamadze <163563288+alexandre-katamadze-Davit-Janashias@users.noreply.github.com>
2024-03-23 22:26:24 +0400

    Add files via upload

---
+++ Goa/day7/day7.py
@@ -0,0 +1,9 @@
+human1 = 9
+human2 = 12
+print(9*12)
+human3 = 18
+print(108/18)
+human4 = 24
+human5 = 30
+print(21+30)
+print(54/6)
---
+++ Goa/full/lesson11.py
@@ -0,0 +1,101 @@
+# გაუშვას კოდის ბლოკი იმდენჯერ რამდენჯერაც გვინდა
+
+# x = საიდან დაიწყოს ათვლა
+# y = სადამდე გაგრძელდეს
+# z = ნაბიჯები, ყოველ ათვლაზე რამდენი ნაბიჯი მოემატოს
+
+#             x   y    z
+# for i in range(5):
+#     print(i)
+
+
+# print(True or False)
+
+# while 10 > 5 or 10 > 15:
+#    print('goa')
+
+
+
+seats = 50
+
+# while seats > 0:
+#    print('ticket is sold')
+#    # seats = seats - 1
+#    seats -= 1
+
+
+
+x = 0
+
+# while x < 10:
+#    print('GOA IS BEST')
+#    x += 1
+
+
+y = 0
+
+while y < 10:
+    print('GOA IS BEST AND NOVATORI IS NOOB')
+    y += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
