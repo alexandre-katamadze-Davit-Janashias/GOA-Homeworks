d2387cb1e77975707d5a13cd49ec5db915b27096
alexandre katamadze <163563288+alexandre-katamadze-Davit-Janashias@users.noreply.github.com>
2024-05-05 00:44:22 +0400

    Add files via upload

---
+++ Goa/day15/day15.py
@@ -0,0 +1,11 @@
+score - int(input("Enter your grade: "))
+
+if score > 90 and score < 100:
+    print("თქვენ დაგიფინანსდებათ სწავლა სრულიად")
+elif score > 70 and score < 80:
+    print("თქვენ დაგიფინანსდებათ სწავლა 1500 ლარით")
+elif score > 40 and score < 70:
+    print("თქვენ დაგიფინანსდებათ სწავლა 500 ლარით")
+
+else:
+    print("თქვენ არ გვჭირდებით")
---
+++ Goa/day15/dayy15.py
@@ -0,0 +1,9 @@
+grade - int(input("Dear Student please enter your math score: "))
+if grade == 10:
+    print("Mr.Joe biden your son is very smart")
+elif grade == 9 or grade == 8:
+    print("Mr.Joe biden your son is smart but he can do more better")
+elif grade == 5:
+    print("Mr.Joe biden you need to control your son")
+elif grade < 5:
+    print("Mr.Joe biden your son is really studpid")
